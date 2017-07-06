c54b24c20525e669b482f4fc3f72d1cdd86cc1ce
Bruno Silvano <bruno_705@hotmail.com>
2017-07-05 22:20:10 -0300

    styling components

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -2,14 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: 'app/events/event-thumbnail.component.html'
+    templateUrl: 'app/events/event-thumbnail.component.html',
+    styles: [`
+        .pad-left { margin-left: 10px; }
+        .well div { color: #bbb; }
+    `]
 })
 export class EventThumbnailComponent {
     @Input() event:any
-
-    someProperty:any = 'some value'
-
-    logFoo() {
-        console.log('foo');
-    }
 }
